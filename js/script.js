fd6de5aed8a51f5dd516ce016c4590c67beaf732
macmillan kimutai <mcmyllano@gmail.com>
2018-03-29 16:13:46 +0300

    added the user interface .

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
   $("form#ping").submit(function(event) {
-    event.preventDefault();
-    var ping = parseInt($("input#number").val());
-    var result = number(ping);
-    $("#result").text(result);
-  });
+  event.preventDefault();
+  var ping = parseInt($("input#ping").val());
+  var result = ping(ping);
+  $("#result").text();
+ });
 });
 
-var ping= function(ping) {
-  return true;
-};
+ for (var x=1; x <= 100; x++){
+   if(x % 3 && x % 5) {
+     document.write(x);
+   } else {
+     if( x % 3 == 0 ) {
+        document.write("ping");
+      }
+      if( x % 5 == 0) {
+        document.write("pong");
+      }
+    }
+    document.write('<br>');
+  }
+
+console.log(ping(x));
